6f117697dd5384b6fc5dad553672cc43657c02ac
FehSouza <fernanda.souza142010@gmail.com>
2024-06-26 21:20:01 -0300

    feat: Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,37 @@
-import { Box } from '@mui/material'
-import { Logo } from 'components/Logo'
+import { Box, Typography } from '@mui/material'
+import { Logo } from 'components'
+import { useTranslation } from 'react-i18next'
 
 export const Footer = () => {
+  const { t } = useTranslation()
+
+  const date = new Date()
+  const year = date.getFullYear()
+
   return (
     <Box data-testid="footer" component="footer" borderTop={1} borderColor="secondary.contrastText">
-      <Box component="section" maxWidth="max-content" mx="auto" px={2} py={1.5}>
+      <Box
+        data-testid="footer-seo"
+        component="section"
+        maxWidth="md"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        gap={3}
+        mx="auto"
+        p={2}
+      >
         <Logo hasText />
+
+        <Typography variant="body2" textAlign="center">
+          {t('Footer.description')}
+        </Typography>
+      </Box>
+
+      <Box data-testid="footer-copyright" component="section" p={2} sx={{ backgroundColor: 'secondary.contrastText' }}>
+        <Typography variant="body2" textAlign="center">
+          {`© ${year} Copyright: GitHub Catalog`}
+        </Typography>
       </Box>
     </Box>
   )
--- src/i18n/languages/index.ts
+++ src/i18n/languages/index.ts
@@ -15,6 +15,11 @@ export const languages = {
         selectLanguage: 'Selecione a linguagem desejada',
       },
 
+      Footer: {
+        description:
+          'Explore e visualize repositórios, seguidores e seguidos de qualquer usuário no GitHub com o GitHub Catalog. Descubra uma ampla gama de projetos para desenvolvedores, tudo em um único lugar organizado. Otimize sua experiência de navegação no GitHub e descubra novas conexões e recursos valiosos. Experimente o GitHub Catalog agora para aprimorar seu fluxo de trabalho de desenvolvimento!',
+      },
+
       Home: {
         'Olá mundo': 'Olá mundo',
       },
@@ -35,6 +40,11 @@ export const languages = {
         portuguese: 'Portuguese',
         english: 'English',
         selectLanguage: 'Select the desired language',
+      },
+
+      Footer: {
+        description:
+          'Explore and visualize repositories, followers, and followed users on GitHub with GitHub Catalog. Discover a wide range of projects for developers, all in one organized place. Enhance your GitHub browsing experience and uncover new connections and valuable resources. Try GitHub Catalog now to streamline your development workflow!',
       },
 
       Home: {
