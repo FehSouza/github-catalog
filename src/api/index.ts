1769cd62b2185430e11e6b63c562c5b9daea1b5c
FehSouza <fernanda.souza142010@gmail.com>
2024-07-04 18:51:48 -0300

    feat: add mocks services

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+export const ENABLE_MOCK = true
+
 export const api = axios.create({
   baseURL: 'https://api.github.com',
 })
--- src/services/getUser/index.ts
+++ src/services/getUser/index.ts
@@ -1,7 +1,9 @@
 import { User } from '@types'
-import { api } from 'api'
+import { api, ENABLE_MOCK } from 'api'
+import { MOCK_GET_USER } from 'mocks'
 
 export const getUser = async (user: string) => {
+  if (ENABLE_MOCK) return MOCK_GET_USER
   const response = await api.get<User>(`/users/${user}`)
   const result = response.data
   return result
